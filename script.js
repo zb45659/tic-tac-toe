b65eac03eaa863a9d4445f8f1c3509d9563df357
zb45659 <benckendorfzac@johndeere.com>
2020-10-07 15:20:23 -0500

    win and draw message added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 ticTacButtons = document.querySelectorAll("a");
 resetButton = document.querySelector("#resetButton")
-
+winMessage = document.querySelector("#message");
 let countClicks = 0;
 
 ticTacButtons.forEach(function changeColor(turn) {
@@ -26,21 +26,31 @@ ticTacButtons.forEach(function changeColor(turn) {
 
 function winLogic() {
     if (ticTacButtons[0].style.backgroundColor === ticTacButtons[1].style.backgroundColor && ticTacButtons[1].style.backgroundColor === ticTacButtons[2].style.backgroundColor && ticTacButtons[0].style.backgroundColor !== "") {
+        winMessage.innerHTML = `${ticTacButtons[0].style.backgroundColor} player won the game`;
         console.log("you win");
-    } else if (ticTacButtons[3].style.backgroundColor === ticTacButtons[4].style.backgroundColor && ticTacButtons[4].style.backgroundColor === ticTacButtons[5].style.backgroundColor) {
+    } else if (ticTacButtons[3].style.backgroundColor === ticTacButtons[4].style.backgroundColor && ticTacButtons[4].style.backgroundColor === ticTacButtons[5].style.backgroundColor && ticTacButtons[3].style.backgroundColor !== "") {
+        winMessage.innerHTML = `${ticTacButtons[3].style.backgroundColor} player won the game`;
         console.log("you win");
     } else if (ticTacButtons[6].style.backgroundColor === ticTacButtons[7].style.backgroundColor && ticTacButtons[7].style.backgroundColor === ticTacButtons[8].style.backgroundColor && ticTacButtons[6].style.backgroundColor !== "") {
+        winMessage.innerHTML = `${ticTacButtons[6].style.backgroundColor} player won the game`;
         console.log("you win");
     } else if (ticTacButtons[0].style.backgroundColor === ticTacButtons[3].style.backgroundColor && ticTacButtons[3].style.backgroundColor === ticTacButtons[6].style.backgroundColor && ticTacButtons[0].style.backgroundColor !== "") {
+        winMessage.innerHTML = `${ticTacButtons[0].style.backgroundColor} player won the game`;
         console.log("you win");
     } else if (ticTacButtons[1].style.backgroundColor === ticTacButtons[4].style.backgroundColor && ticTacButtons[4].style.backgroundColor === ticTacButtons[7].style.backgroundColor && ticTacButtons[1].style.backgroundColor !== "") {
+        winMessage.innerHTML = `${ticTacButtons[1].style.backgroundColor} player won the game`;
         console.log("you win");
     } else if (ticTacButtons[2].style.backgroundColor === ticTacButtons[5].style.backgroundColor && ticTacButtons[5].style.backgroundColor === ticTacButtons[8].style.backgroundColor && ticTacButtons[2].style.backgroundColor !== "") {
+        winMessage.innerHTML = `${ticTacButtons[2].style.backgroundColor} player won the game`;
         console.log("you win");
     } else if (ticTacButtons[0].style.backgroundColor === ticTacButtons[4].style.backgroundColor && ticTacButtons[4].style.backgroundColor === ticTacButtons[8].style.backgroundColor && ticTacButtons[0].style.backgroundColor !== "") {
+        winMessage.innerHTML = `${ticTacButtons[0].style.backgroundColor} player won the game`;
         console.log("you win");
     } else if (ticTacButtons[2].style.backgroundColor === ticTacButtons[4].style.backgroundColor && ticTacButtons[4].style.backgroundColor === ticTacButtons[6].style.backgroundColor && ticTacButtons[2].style.backgroundColor !== "") {
+        winMessage.innerHTML = `${ticTacButtons[2].style.backgroundColor} player won the game!`;
         console.log("you win");
+    } else if (countClicks == 9) {
+        winMessage.innerHTML = `You both lose!`;
     }
 }
     
